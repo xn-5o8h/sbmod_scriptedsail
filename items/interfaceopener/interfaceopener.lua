e48f8e132cb4d5c86c63dc2783787b6b1f69690c
xn-5o8h <cyelae+gh@vivaldi.net>
2018-02-09 05:04:46 +0100

    Actually deleted them. I'm git noob

--- items/interfaceopener/interfaceopener.lua
+++
@@ -1,4 +0,0 @@
-function init()
-	activeItem.interact(config.getParameter("interactAction"), config.getParameter("interactData"), config.getParameter("techStationId"))
-	player.setSwapSlotItem(config.getParameter("swapItem"))
-end
--- scripts/companionhandler.lua
+++
@@ -1,7 +0,0 @@
-local scriptedailOldInit = init
-
-function init()
-  scriptedailOldInit()
-  message.setHandler("returnCompanions", function() return playerCompanions.getCompanions("crew") end)
-  message.setHandler("dismissCompanion", function(_, _, podUuid) recruitSpawner:dismiss(podUuid) end)
-end
