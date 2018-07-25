2ec07647f7dcbeb04d36435d060024d1c192a02f
xn-5o8h <cyelae+gh@vivaldi.net>
2018-07-25 17:36:43 +0200

    Fix portrait.lua for quest log Add preview on the ancient chips Add portrait on the ancient chips

--- scriptedArtificialIntelligenceLattice/quests/scripts/portraits.lua
+++ scriptedArtificialIntelligenceLattice/quests/scripts/portraits.lua
@@ -1,11 +1,25 @@
+-- this is only started once on new quests and on init
+-- so it isn't updated when someone change their A.I. chip until they change world
+
 function setPortraits(titleFn)
-  local getStoredSAILportrait = world.sendEntityMessage(player.id(),"getDefaults", "SAILportrait", nil)
+  local SAILportrait = status.statusProperty("SAILportraitDefaults")
+  local SAILportraitPath = string.format("/ai/portraits/%squestportrait.png", player.species())
+
+  if SAILportrait then
+    -- having it so hardcoded was dumb but I can't really turn back now that some people made their own chips
+    -- so we naively look if the saved SAIL portrait is a path since / is forbidden in filenames
+    if string.match(SAILportrait, "/") then 
+      SAILportraitPath = SAILportrait
+    else
+      SAILportraitPath = string.format("/ai/portraits/%squestportrait.png", SAILportrait)
+    end
+  end
 
   quest.setParameter("sail", {
     type = "noDetail",
     name = "S.A.I.L",
     portrait = {
-      { image = string.format("/ai/portraits/%squestportrait.png", getStoredSAILportrait:result() or player.species()) }
+      { image = SAILportraitPath }
     }
   })
 
